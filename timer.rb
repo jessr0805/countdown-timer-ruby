4de6e4c84ce5968a08191c7c02ed23fe240b0110
jessr2013 <jessesodhi26@gmail.com>
2026-08-15 18:05:31 -0400

    feat: enable users to input seconds for timer countdown

--- timer.rb
+++ timer.rb
@@ -1,4 +1,10 @@
-countdown = [5, 4, 3, 2, 1]
+puts "===================="
+puts "     Ruby Timer     "
+puts "===================="
+puts "How many seconds would you like to set the timer for?"
+seconds = gets.chomp.to_i
+sleep(1)
+countdown = seconds.downto(1)
 countdown.each do |number|
   puts number
   sleep(1)
